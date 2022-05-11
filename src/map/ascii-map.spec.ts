b4aadf716855f2e4352f1e8ee2c16d7c62928e92
matej <matejb6@icloud.com>
2022-05-11 21:40:39 +0200

    files formatted

--- src/map/ascii-map.spec.ts
+++ src/map/ascii-map.spec.ts
@@ -7,24 +7,22 @@ import { map1 } from '../../data/valid-maps';
 import { map6, map7, map8, map9, map10 } from '../../data/invalid-maps';
 
 describe('Test AsciiMap class', () => {
+  it('should have map', () => {
+    expect(new AsciiMap(map1).getAsciiMap()).to.equal(map1);
+  });
 
-    it('should have map', () => {
-        expect(new AsciiMap(map1).getAsciiMap()).to.equal(map1);
-    });
+  it('should have map points', () => {
+    const asciiMap = new AsciiMap(map1);
+    expect(asciiMap.getAsciiMapPoints().length).to.be.greaterThan(0);
+  });
 
-    it('should have map points', () => {
-        const asciiMap = new AsciiMap(map1);
-        expect(asciiMap.getAsciiMapPoints().length).to.be.greaterThan(0);
-    });
-
-    it('should have valid map check', () => {
-        expect(new AsciiMap(map1).isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(true);
-        expect(new AsciiMap(map6).isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(false);
-        expect(new AsciiMap(map7).isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(false);
-        expect(new AsciiMap(map8).isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(false);
-        expect(new AsciiMap(map9).isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(false);
-        expect(new AsciiMap(map10).isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(false);
-        expect(new AsciiMap('').isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(false);
-    });
-
+  it('should have valid map check', () => {
+    expect(new AsciiMap(map1).isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(true);
+    expect(new AsciiMap(map6).isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(false);
+    expect(new AsciiMap(map7).isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(false);
+    expect(new AsciiMap(map8).isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(false);
+    expect(new AsciiMap(map9).isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(false);
+    expect(new AsciiMap(map10).isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(false);
+    expect(new AsciiMap('').isMapValid(constants.pathStartChar, constants.pathEndChar)).to.equal(false);
+  });
 });
--- src/point/ascii-map-point.spec.ts
+++ src/point/ascii-map-point.spec.ts
@@ -3,20 +3,18 @@ import { expect } from 'chai';
 import { AsciiMapPoint } from './ascii-map-point';
 
 describe('Test AsciiMapPoint class', () => {
+  let asciiMapPoint: AsciiMapPoint;
 
-    let asciiMapPoint: AsciiMapPoint;
+  beforeEach(() => {
+    asciiMapPoint = new AsciiMapPoint(1, 2, 'A');
+  });
 
-    beforeEach(() => {
-        asciiMapPoint = new AsciiMapPoint(1, 2, 'A');
-    });
+  it('should have position', () => {
+    expect(asciiMapPoint.getX()).to.equal(1);
+    expect(asciiMapPoint.getY()).to.equal(2);
+  });
 
-    it('should have position', () => {
-        expect(asciiMapPoint.getX()).to.equal(1);
-        expect(asciiMapPoint.getY()).to.equal(2);
-    });
-
-    it('should have value', () => {
-        expect(asciiMapPoint.getValue()).to.equal('A');
-    });
-
+  it('should have value', () => {
+    expect(asciiMapPoint.getValue()).to.equal('A');
+  });
 });
