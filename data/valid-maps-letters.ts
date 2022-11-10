22066cdfae36a9f042a748c3b5fbed6c696dba2e
Matej Buljan <matejb6@icloud.com>
2022-11-10 12:55:10 +0100

    Refactor data

--- data/valid-maps-letters.ts
+++
@@ -1,7 +0,0 @@
-export const map1Letters = 'ACB';
-export const map2Letters = 'ABCD';
-export const map3Letters = 'ACB';
-export const map4Letters = 'ABCD';
-export const map5Letters = 'ABCD';
-
-export const validMapsLetters: string[] = [map1Letters, map2Letters, map3Letters, map4Letters, map5Letters];
--- data/valid-maps-path-as-chars.ts
+++
@@ -1,7 +0,0 @@
-export const map1PathAsChars = '@---A---+|C|+---+|+-B-x';
-export const map2PathAsChars = '@|A+---B--+|+--C-+|-||+---D--+|x';
-export const map3PathAsChars = '@---A---+|||C---+|+-B-x';
-export const map4PathAsChars = '@--A-+|+-+|A|+--B--+C|+-+|+-C-+|D|x';
-export const map5PathAsChars = '@A+++A|+-B-+C+++C-+Dx';
-
-export const validMapsPathAsChars: string[] = [map1PathAsChars, map2PathAsChars, map3PathAsChars, map4PathAsChars, map5PathAsChars];
