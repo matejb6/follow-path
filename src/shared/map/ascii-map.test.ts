f18fb7c165f87a7f9b8c71035dce7cbe01ca53e8
Matej Buljan <matejb6@icloud.com>
2022-11-10 12:57:34 +0100

    Refactor tests

--- src/shared/map/ascii-map.test.ts
+++ src/shared/map/ascii-map.test.ts
@@ -1,23 +1,21 @@
-import { AsciiMap } from './ascii-map';
-import { constants } from '../../core/constants/constants';
 import { map1 } from '../../../data/valid-maps';
-import { map6 } from '../../../data/invalid-maps';
+import { invalidMap1 } from '../../../data/invalid-maps';
+import { AsciiMap } from './ascii-map';
 
 describe('AsciiMap', () => {
   test('should have map points', () => {
-    const asciiMap = new AsciiMap(map1);
-    expect(asciiMap.mapPoints.length).toBeGreaterThan(0);
+    expect(new AsciiMap(map1).mapPoints.length).toBeGreaterThan(0);
   });
 
-  test('should check valid map', () => {
-    expect(new AsciiMap(map1).isMapValid(constants.pathStartChar, constants.pathEndChar)).toEqual(true);
+  test('should mark map as valid', () => {
+    expect(new AsciiMap(map1).isMapValid()).toEqual(true);
   });
 
-  test('should check invalid map', () => {
-    expect(new AsciiMap(map6).isMapValid(constants.pathStartChar, constants.pathEndChar)).toEqual(false);
+  test('should mark map as invalid', () => {
+    expect(new AsciiMap(invalidMap1).isMapValid()).toEqual(false);
   });
 
-  test('should check empty map as invalid', () => {
-    expect(new AsciiMap('').isMapValid(constants.pathStartChar, constants.pathEndChar)).toEqual(false);
+  test('should mark empty map as invalid', () => {
+    expect(new AsciiMap('').isMapValid()).toEqual(false);
   });
 });
--- tests/acceptance/acceptance.test.ts
+++ tests/acceptance/acceptance.test.ts
@@ -1,38 +1,98 @@
+import { map1, map2, map3, map4, map5 } from '../../data/valid-maps';
+import { invalidMap1, invalidMap2, invalidMap3, invalidMap4, invalidMap5 } from '../../data/invalid-maps';
 import { AsciiMap } from '../../src/shared/map/ascii-map';
 import { Path } from '../../src/shared/path/path';
-import { validMaps } from '../../data/valid-maps';
-import { validMapsLetters } from '../../data/valid-maps-letters';
-import { validMapsPathAsChars } from '../../data/valid-maps-path-as-chars';
-import { invalidMaps } from '../../data/invalid-maps';
-import { constants } from '../../src/core/constants/constants';
 
 describe('Acceptance test', () => {
-  describe('collected letters', () => {
-    validMaps.forEach((map: string, index: number) => {
-      test('should have map collected letters', () => {
-        const asciiMap = new AsciiMap(map);
-        const path = new Path(asciiMap);
-        expect(path.collectLetters(constants.alphabet)).toEqual(validMapsLetters[index]);
-      });
+  describe('valid maps', () => {
+    test('should mark a basic map as valid', () => {
+      expect(new AsciiMap(map1).isMapValid()).toEqual(true);
+    });
+    test('should mark a map with go straight through intersections as valid', () => {
+      expect(new AsciiMap(map2).isMapValid()).toEqual(true);
+    });
+    test('should mark a map with letters on turns as valid', () => {
+      expect(new AsciiMap(map3).isMapValid()).toEqual(true);
+    });
+    test('should mark a map with intersections through letters as valid', () => {
+      expect(new AsciiMap(map4).isMapValid()).toEqual(true);
+    });
+    test('should mark a compact map without space between letters and turns as valid', () => {
+      expect(new AsciiMap(map5).isMapValid()).toEqual(true);
     });
   });
 
-  describe('path as chars', () => {
-    validMaps.forEach((map: string, index: number) => {
-      test('should have map path as chars', () => {
-        const asciiMap = new AsciiMap(map);
-        const path = new Path(asciiMap);
-        expect(path.getPathAsString()).toEqual(validMapsPathAsChars[index]);
-      });
+  describe('letter collection', () => {
+    test('should collect letters in map 1', () => {
+      const asciiMap = new AsciiMap(map1);
+      const path = new Path(asciiMap);
+      expect(path.collectLetters()).toEqual('ACB');
+    });
+    test('should collect letters in map 2', () => {
+      const asciiMap = new AsciiMap(map2);
+      const path = new Path(asciiMap);
+      expect(path.collectLetters()).toEqual('ABCD');
+    });
+    test('should collect letters in map 3', () => {
+      const asciiMap = new AsciiMap(map3);
+      const path = new Path(asciiMap);
+      expect(path.collectLetters()).toEqual('ACB');
+    });
+    test('should collect letters in map 4', () => {
+      const asciiMap = new AsciiMap(map4);
+      const path = new Path(asciiMap);
+      expect(path.collectLetters()).toEqual('ABCD');
+    });
+    test('should collect letters in map 5', () => {
+      const asciiMap = new AsciiMap(map5);
+      const path = new Path(asciiMap);
+      expect(path.collectLetters()).toEqual('ABCD');
     });
   });
 
-  describe('invalid maps errors', () => {
-    invalidMaps.forEach((map: string) => {
-      test('should have map path as chars', () => {
-        const asciiMap = new AsciiMap(map);
-        expect(asciiMap.isMapValid(constants.pathStartChar, constants.pathEndChar)).toEqual(false);
-      });
+  describe('path as characters', () => {
+    test('should have path as characters in map 1', () => {
+      const asciiMap = new AsciiMap(map1);
+      const path = new Path(asciiMap);
+      expect(path.getPathAsString()).toEqual('@---A---+|C|+---+|+-B-x');
+    });
+    test('should have path as characters in map 2', () => {
+      const asciiMap = new AsciiMap(map2);
+      const path = new Path(asciiMap);
+      expect(path.getPathAsString()).toEqual('@|A+---B--+|+--C-+|-||+---D--+|x');
+    });
+    test('should have path as characters in map 3', () => {
+      const asciiMap = new AsciiMap(map3);
+      const path = new Path(asciiMap);
+      expect(path.getPathAsString()).toEqual('@---A---+|||C---+|+-B-x');
+    });
+    test('should have path as characters in map 4', () => {
+      const asciiMap = new AsciiMap(map4);
+      const path = new Path(asciiMap);
+      expect(path.getPathAsString()).toEqual('@--A-+|+-+|A|+--B--+C|+-+|+-C-+|D|x');
+    });
+    test('should have path as characters in map 5', () => {
+      const asciiMap = new AsciiMap(map5);
+      const path = new Path(asciiMap);
+      expect(path.getPathAsString()).toEqual('@A+++A|+-B-+C+++C-+Dx');
+    });
+  });
+
+  describe('invalid maps', () => {
+    test('should mark a map without starting point as invalid', () => {
+      expect(new AsciiMap(invalidMap1).isMapValid()).toEqual(false);
+    });
+    test('should mark a map without ending point as invalid', () => {
+      expect(new AsciiMap(invalidMap2).isMapValid()).toEqual(false);
+    });
+    test('should mark a map with multiple starting points as invalid', () => {
+      expect(new AsciiMap(invalidMap3).isMapValid()).toEqual(false);
+    });
+    test('should mark a map with multiple ending points as invalid', () => {
+      expect(new AsciiMap(invalidMap4).isMapValid()).toEqual(false);
+    });
+    test('should mark a map with T fork as invalid', () => {
+      expect(new AsciiMap(invalidMap5).isMapValid()).toEqual(false);
     });
   });
 });
